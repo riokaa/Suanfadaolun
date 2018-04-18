0d373d57b518981b5bcf6b2d82ed02eef340d4f0
rayiooo <rayiooo@foxmail.com>
2018-04-18 18:39:25 +0800

    exp2 v1.0

--- src/Exp1/Main.java
+++ src/Exp1/Main.java
@@ -1,15 +1,22 @@
 package Exp1;
 
+/*
+ * Experience 1：
+ * 生成一个100个点、3000条边的有向随机图
+ * 任选一点为源点，计算s到其它点的距离
+ * 注：图用邻接链表存储
+ */
+
 public class Main {
 	/*
 	 * 主程序从这里开始运行
 	 */
 	public static void main(String[] args) {
 		System.out.println("要输出的内容");
-		Graph g = new Graph(100);//生成100个点
-		g.randomNewEdge(3000);//生成3000个边
-		g.print();//输出图的邻接链表
-		g.getDistanceOfEveryPoints(50);
+		Graph g = new Graph(100);	//生成100个点
+		g.randomNewEdge(3000);		//生成3000个边
+		g.print();					//输出图的邻接链表
+		g.getDistanceOfEveryPoints(50);	//计算每个点到50号点的距离
 
 	}
 
---
+++ src/Exp2/Main.java
@@ -0,0 +1,18 @@
+package Exp2;
+
+/*
+ * Experience 2：
+ * 将一个（100个点，500条边）的有向图
+ * 变成有向无环图（DAG）
+ * 提示：用DFS，但不允许用递归
+ */
+
+public class Main {
+	/*
+	 * program begin from here
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
